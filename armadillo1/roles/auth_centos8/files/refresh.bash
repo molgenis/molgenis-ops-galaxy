70bfd6bad3e58412e5e01f0c340a12b7f1d89384
Sido Haakma <sido@haakma.org>
2021-02-12 09:02:39 +0100

    feat: added auth role to armadillo

---
+++ armadillo1/roles/auth_centos8/files/refresh.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+cd /usr/share/armadillo/auth
+# check if rserver is known in systemctl, if that is the case stop it, and remove it safely
+if  systemctl list-unit-files | grep armadillo-auth
+then
+  systemctl stop armadillo-auth
+  systemctl disable armadillo-auth
+  rm -f /etc/systemd/system/armadillo-auth.service
+  systemctl daemon-reload
+  podman rm armadillo-auth
+fi
+
+/usr/share/armadillo/auth/start.bash
+podman generate systemd --name armadillo-auth > /etc/systemd/system/armadillo-auth.service
+podman stop armadillo-auth
+systemctl daemon-reload
+systemctl enable armadillo-auth
+systemctl start armadillo-auth
